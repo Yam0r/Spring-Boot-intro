4063ccab4f85be21545a1219a5e4e13506aa0213
Yarolsav <kiritok712@gmail.com>
2024-06-21 11:02:36 +0200

    part1

---
+++ src/main/java/mate/academy/springintro/repository/BookService.java
@@ -0,0 +1,9 @@
+package mate.academy.springintro.repository;
+
+import java.awt.print.Book;
+import java.util.List;
+
+public interface BookService {
+    Book save(Book book);
+    List<Book> findAll();
+}
---
+++ src/main/java/mate/academy/springintro/repository/Impl/BookServiceImpl.java
@@ -0,0 +1,31 @@
+package mate.academy.springintro.repository.Impl;
+
+
+import mate.academy.springintro.repository.BookRepository;
+import mate.academy.springintro.repository.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.awt.print.Book;
+import java.util.List;
+
+@Service
+public class BookServiceImpl implements BookService {
+
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookServiceImpl(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public Book save(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return List.of();
+    }
+}
