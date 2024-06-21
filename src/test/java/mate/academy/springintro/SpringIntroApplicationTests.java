83378c3ec2c66c2b88cd42b48657de364ecb5884
Yarolsav <kiritok712@gmail.com>
2024-06-21 10:15:10 +0200

    helloWorld

---
+++ src/test/java/mate/academy/springintro/SpringIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package mate.academy.springintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
