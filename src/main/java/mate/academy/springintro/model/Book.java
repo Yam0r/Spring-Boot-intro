77f30e2407bf12c99418ba8b933a4e3a9a87252f
Yarolsav <kiritok712@gmail.com>
2024-07-04 17:30:29 +0200

    save_exercice

--- src/main/java/mate/academy/springintro/model/Book.java
+++ src/main/java/mate/academy/springintro/model/Book.java
@@ -34,5 +34,7 @@ public class Book {
     private String description;
 
     private String coverImage;
-}
 
+    @Column(nullable = false)
+    private Boolean deleted = false;
+}
--- src/main/java/mate/academy/springintro/service/BookRepository.java
+++ src/main/java/mate/academy/springintro/service/BookRepository.java
@@ -1,13 +1,15 @@
 package mate.academy.springintro.service;
 
 import java.util.List;
+import java.util.Optional;
+
 import mate.academy.springintro.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
-    Book save(Book book);
-
-    List<Book> findAll();
+    List<Book> findByDeletedFalse();
+    Optional<Book> findByIdAndDeletedFalse(Long id);
 }
+
