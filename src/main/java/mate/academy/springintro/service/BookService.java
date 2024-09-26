e25043cfee0a341cd727ad0991ed893dfe39af47
Yarolsav <kiritok712@gmail.com>
2024-09-26 11:29:36 +0200

    2024-09-26T11:14:06.063+02:00 ERROR 10780 --- [nio-8080-exec-7] o.a.c.c.C.[.[.[.[dispatcherServlet]      : Servlet.service() for servlet [dispatcherServlet] in context with path [/api] threw exception [Request processing failed: org.springframework.data.mapping.PropertyReferenceException: No property 'string' found for type 'Book'] with root cause

--- src/main/java/mate/academy/springintro/service/BookService.java
+++ src/main/java/mate/academy/springintro/service/BookService.java
@@ -4,11 +4,12 @@ import java.util.List;
 import mate.academy.springintro.dto.BookDto;
 import mate.academy.springintro.dto.BookSearchParameters;
 import mate.academy.springintro.dto.CreateBookRequestDto;
+import org.springframework.data.domain.Pageable;
 
 public interface BookService {
     BookDto save(CreateBookRequestDto requestDto);
 
-    List<BookDto> findAll();
+    List<BookDto> findAll(Pageable pageable);
 
     BookDto findById(Long id);
 
--- src/main/java/mate/academy/springintro/service/impl/BookServiceImpl.java
+++ src/main/java/mate/academy/springintro/service/impl/BookServiceImpl.java
@@ -10,6 +10,7 @@ import mate.academy.springintro.model.Book;
 import mate.academy.springintro.repository.book.BookRepository;
 import mate.academy.springintro.repository.book.BookSpecificationBuilder;
 import mate.academy.springintro.service.BookService;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
@@ -35,8 +36,8 @@ public class BookServiceImpl implements BookService {
     }
 
     @Override
-    public List<BookDto> findAll() {
-        return bookRepository.findAll().stream()
+    public List<BookDto> findAll(Pageable pageable) {
+        return bookRepository.findAll(pageable).stream()
                 .map(bookMapper::toBookDto)
                 .toList();
     }
