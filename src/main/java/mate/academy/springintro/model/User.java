4f25efc7a942dfcebe84a8c12025a1b1894c5435
Yarolsav <kiritok712@gmail.com>
2024-10-21 13:24:43 +0200

    add SQLRestriction

--- src/main/java/mate/academy/springintro/model/User.java
+++ src/main/java/mate/academy/springintro/model/User.java
@@ -9,13 +9,14 @@ import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 import org.hibernate.annotations.Where;
 
 @Getter
 @Setter
 @Entity
-@SQLDelete(sql = "UPDATE users SET is_deleted = true WHERE id=?")
-@Where(clause = "is_deleted = false")
+@SQLDelete(sql = "UPDATE users SET is_deleted = TRUE WHERE id = ?")
+@SQLRestriction(value = "is_deleted = FALSE")
 @Table(name = "users")
 public class User {
     @Id
