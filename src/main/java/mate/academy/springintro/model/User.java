c14b7f801ec36362ab970f2ddeab7d181ba47c43
Yarolsav <kiritok712@gmail.com>
2024-10-21 13:30:33 +0200

    fix styles

--- src/main/java/mate/academy/springintro/model/User.java
+++ src/main/java/mate/academy/springintro/model/User.java
@@ -10,7 +10,6 @@ import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
-import org.hibernate.annotations.Where;
 
 @Getter
 @Setter
