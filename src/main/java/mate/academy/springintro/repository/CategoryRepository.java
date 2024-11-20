8fbc0ed03bb5f007dcbee7d888f6d8112ad350b3
Yarolsav <kiritok712@gmail.com>
2024-11-20 18:27:03 +0100

    squash! fix style errors

--- src/main/java/mate/academy/springintro/repository/CategoryRepository.java
+++ src/main/java/mate/academy/springintro/repository/CategoryRepository.java
@@ -1,7 +1,6 @@
 package mate.academy.springintro.repository;
 
 import java.util.List;
-import java.util.Optional;
 import mate.academy.springintro.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
